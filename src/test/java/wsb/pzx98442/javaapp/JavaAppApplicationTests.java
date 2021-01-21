3ef383fce64a515fb551cda58b6e5dc37fc659dc
Piasecki Lukasz <lukasz.piasecki@the-nature-network.pl>
2021-01-21 17:32:18 +0100

    Initial commit

---
+++ src/test/java/wsb/pzx98442/javaapp/JavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.pzx98442.javaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
